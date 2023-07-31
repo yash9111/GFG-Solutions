e506dccd961f1e25bdaed03bca4259d69e739a30
Yash pathak <assush698@gmail.com>
2023-07-31 09:04:06 +0530

    solved

---
+++ 31July.cpp
@@ -0,0 +1,17 @@
+  vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+         vector<int> ans,vis(V,0);
+        queue<int> q;
+        q.push(0);vis[0]++;
+        while(!q.empty()){
+            int d = q.front();q.pop();
+            ans.push_back(d);
+            for(int i:adj[d]){
+                if(!vis[i]){
+                    q.push(i);
+                    vis[i]++;
+                }
+            }
+        }
+        return ans;
+    }
