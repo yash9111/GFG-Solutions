fcc625b88b6cfcbfdc22adf1c60c9018fad56c99
Yash pathak <assush698@gmail.com>
2023-08-07 17:15:21 +0530

    solved

--- August Solutions/7Aug.cpp
+++ August Solutions/7Aug.cpp
@@ -1,83 +1,69 @@
 class Solution{
-    
-    static int getRow(int num) {
-        return num / 9;    
-    }
-    
-    static int getCol(int num) {
-        return num % 9;
-    }
-    
-    static int getGrpRow(int row) {
-        return row / 3;
-    }
-    
-    static int getGrpCol(int col) {
-        return col / 3;
-    }
-    
-    static boolean solve(int pos, int arr[][], Set<Integer> group[][], Set<Integer> row[], Set<Integer> col[]) {
-        if(pos == 81) return true;
-        
-        int r = getRow(pos), c = getCol(pos);
-        
-        if(arr[r][c] != 0) return solve(pos+1, arr, group, row, col);
-        int gr = getGrpRow(r), gc = getGrpCol(c);
-        
-        for(int put=1;put<10;++put){
-            if(!row[r].contains(put) && !col[c].contains(put) && !group[gr][gc].contains(put)) {
-                row[r].add(put);
-                col[c].add(put);
-                group[gr][gc].add(put);
-                boolean ans = solve(pos+1, arr, group, row, col);
-                row[r].remove(put);
-                col[c].remove(put);
-                group[gr][gc].remove(put);
-                if(ans == true) {
-                    arr[r][c] = put;
+    public:
+    bool FindUnassignedLocation(int grid[N][N], int &row, int &col) {
+        for (row = 0; row < N; row++)
+            for (col = 0; col < N; col++)
+                if (grid[row][col] == UNASSIGNED)
                     return true;
-                }
-            }
-        }
-        
         return false;
     }
-    static boolean SolveSudoku(int grid[][]){
-        Set<Integer> group[][] = new Set[3][3];
-        Set<Integer> row[] = new Set[9];
-        Set<Integer> col[] = new Set[9];
-        
-        for(int i=0;i<group.length;++i){
-           for(int j=0;j<group[0].length;++j) group[i][j] = new HashSet<Integer>();
-        }
-        
-        for(int i=0;i<row.length;++i) row[i] = new HashSet<Integer>();
-        for(int i=0;i<col.length;++i) col[i] = new HashSet<Integer>(); 
-        
-        for(int i=0;i<grid.length;++i){
-            for(int j=0;j<grid[0].length;++j){
-                if(grid[i][j] == 0) continue;
-                group[i/3][j/3].add(grid[i][j]);
-                row[i].add(grid[i][j]);
-                col[j].add(grid[i][j]);
-            }
-        }
-        
-        boolean ans = solve(0, grid, group, row, col);
-        
-        if(ans){
-            return true;
-        }
-        else{
-            return false;
-        }
+    
+    bool UsedInRow(int grid[N][N], int row, int num) {
+        for (int col = 0; col < N; col++)
+            if (grid[row][col] == num)
+                return true;
+        return false;
     }
     
-    static void printGrid (int grid[][]){
-        for(int i=0;i<grid.length;++i){
-            for(int j=0;j<grid[0].length;++j){
-                System.out.print(grid[i][j]+" ");
+    bool UsedInCol(int grid[N][N], int col, int num) {
+        for (int row = 0; row < N; row++)
+            if (grid[row][col] == num)
+                return true;
+        return false;
+    }
+    
+    bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num) {
+        for (int row = 0; row < 3; row++)
+            for (int col = 0; col < 3; col++)
+                if (grid[row + boxStartRow][col + boxStartCol] == num)
+                    return true;
+        return false;
+    }
+    
+    bool isSafe(int grid[N][N], int row, int col, int num) {
+        return !UsedInRow(grid, row, num) &&
+               !UsedInCol(grid, col, num) &&
+               !UsedInBox(grid, row - row % 3 , col - col % 3, num) &&
+               grid[row][col] == UNASSIGNED;
+    }
+    
+    //Function to find a solved Sudoku. 
+    bool SolveSudoku(int grid[N][N])  
+    { 
+        // Your code here
+        int row, col;
+        if (!FindUnassignedLocation(grid, row, col))
+            return true; // success!
+    
+        for (int num = 1; num <= 9; num++) {
+            if (isSafe(grid, row, col, num)) {
+                grid[row][col] = num;
+                if (SolveSudoku(grid))
+                    return true;
+                grid[row][col] = UNASSIGNED; // failure, undo
             }
         }
+        return false; // trigger backtracking
     }
-}
+    
+    //Function to print grids of the Sudoku.
+    void printGrid (int grid[N][N]) 
+    {
+        // Your code here 
+        for (int row = 0; row < N; row++) {
+        for (int col = 0; col < N; col++) {
+            cout << grid[row][col] << " ";
+        }
+    }
+    }
+};
