1407b5b008269070be58dc609ed1e0f505db05b7
Yash pathak <assush698@gmail.com>
2023-08-12 07:58:35 +0530

    Solved

---
+++ August Solutions/12Aug.cpp
@@ -0,0 +1,16 @@
+ int longestSubsequence(int n, int a[])
+    {
+       
+         vector<int> lis;
+        
+        for (int i = 0; i < n; i++)
+        {
+            auto it = lower_bound(lis.begin(), lis.end(), a[i]);
+            if (it == lis.end())
+                lis.push_back(a[i]);
+            else
+                *it = a[i];
+        }
+        
+        return lis.size();
+    }
